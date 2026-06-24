1afdb020f3324d88362b430d85c53fbdca142a38
Yohan3128 <Yohansilva560@gmail.com>
2026-06-24 11:08:14 +0530

    Add VCS configuration files for FirstClient and SecondClient

---
+++ JMS-SecondClient/src/main/java/com/hnys/jms/DefaultConnection.java
@@ -0,0 +1,39 @@
+package com.hnys.jms;
+
+import jakarta.jms.*;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class DefaultConnection {
+    public static void main(String[] args) {
+        try {
+            InitialContext context = new InitialContext();
+            ConnectionFactory factory = (ConnectionFactory) context.lookup("jms/__defaultConnectionFactory");
+
+            Connection connection = factory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+           Topic topic= (Topic) context.lookup("myTopic");
+
+            MessageConsumer consumer = session.createConsumer(topic);
+            consumer.setMessageListener(new MessageListener() {
+                public void onMessage(Message message) {
+                    try {
+                        System.out.println(message.getBody(String.class));
+                    } catch (JMSException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+
+
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
