a786c8f4cf0a46f56980af83ac800fcd26efa3f0
Yohan3128 <Yohansilva560@gmail.com>
2026-06-24 11:22:04 +0530

    Enhance project configuration and implement message handling
    - Update .idea/misc.xml to include ExternalStorageConfigurationManager and ProjectRootManager settings.
    - Modify pom.xml to add project name and Jakarta EE dependency.
    - Implement MessageReceiver class for JMS message handling.
    - Create Test servlet for handling HTTP requests.
    - Update index.jsp with a proper title structure.
    - Add encodings.xml for source file encoding configuration.
    - Introduce vcs.xml for version control system mappings.

--- jms-webapp/src/main/java/com/hnys/jms/MessageReceiver.java
+++ jms-webapp/src/main/java/com/hnys/jms/MessageReceiver.java
@@ -1,4 +1,28 @@
 package com.hnys.jms;
 
-public class MessageReceiver {
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.ActivationConfigProperty;
+import jakarta.ejb.MessageDriven;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationLookup",propertyValue = "myTopic")
+})
+public class MessageReceiver implements MessageListener {
+
+    @PostConstruct
+    public void init(){
+        System.out.println("MessageReceiver init");
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            System.out.println("MessageReceiver onMessage: "+message.getBody(String.class));
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- jms-webapp/src/main/java/com/hnys/jms/Test.java
+++ jms-webapp/src/main/java/com/hnys/jms/Test.java
@@ -1,4 +1,17 @@
 package com.hnys.jms;
 
-public class Test {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
