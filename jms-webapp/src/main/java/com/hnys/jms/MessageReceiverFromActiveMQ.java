7fd56d4fc7ea6d217ee09a0eaa682dfbf8f534a6
Yohan3128 <Yohansilva560@gmail.com>
2026-06-24 14:23:48 +0530

    Implement MessageReceiverFromActiveMQ as a MessageDriven bean for JMS message handling

--- jms-webapp/src/main/java/com/hnys/jms/MessageReceiverFromActiveMQ.java
+++ jms-webapp/src/main/java/com/hnys/jms/MessageReceiverFromActiveMQ.java
@@ -1,4 +1,25 @@
 package com.hnys.jms;
 
-public class MessageReceiverFromActiveMQ {
+import jakarta.ejb.ActivationConfigProperty;
+import jakarta.ejb.MessageDriven;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType",propertyValue = "jakarta.jms.Topic"),
+        @ActivationConfigProperty(propertyName = "destinationLookup",propertyValue = "activemqTopic"),
+        @ActivationConfigProperty(propertyName = "destination",propertyValue = "activemqTopic"),
+        @ActivationConfigProperty(propertyName = "resourceAdapter",propertyValue = "activemq-rar-6.2.6")
+})
+public class MessageReceiverFromActiveMQ implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+        try {
+            System.out.println("Message : " + message.getBody(String.class));
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
